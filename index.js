0a2a0ed3f2598999fb9feaacd997aff1d4019b75
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-08-15 11:38:21 -0700

    index.js added

---
+++ index.js
@@ -0,0 +1,28 @@
+// set integer for treasure 
+var treasureLocation = Math.floor(Math.random() * 16)
+// set  2 integers for trap taht are not overlapping with treasure or each other
+
+var trap1Location = Math.floor(Math.random() * 16);
+
+
+
+if (trap1Location === treasureLocation){
+    trap1Location = Math.floor(Math.random() * 16);
+}
+
+// }
+ const treasure = (location) => {
+     if (treasureLocation === location){
+         document.getElementById(location).innerHTML = "👑";
+         
+         document.getElementById("arg").innerHTML = "YOU WIN! YOU FOUND THE TREASURE"
+     }
+     else if (location === trap1Location) {
+       document.getElementById(location).innerHTML = "☠️";
+       document.getElementById("arg").innerHTML = "You fell in a trap!  Try Again?";
+     }
+     else {
+       document.getElementById(location).innerHTML = "✘";
+     }
+ }
+ 
