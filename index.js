2ebef519c26520cab7e1e45c686ea64831cd158f
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-08-15 14:03:35 -0700

    notes for jumpstart

--- index.js
+++ index.js
@@ -4,24 +4,32 @@ var treasureLocation = Math.floor(Math.random() * 16)
 
 var trap1Location = Math.floor(Math.random() * 16);
 
-
+var counter = 5
 
 if (trap1Location === treasureLocation){
     trap1Location = Math.floor(Math.random() * 16);
 }
 
+const restart = () =>{
+  location.reload()
+}
+
 // }
  const treasure = (location) => {
      if (treasureLocation === location){
-         document.getElementById(location).innerHTML = "👑";
+         document.getElementById(location).innerHTML = "🏆";
          
-         document.getElementById("arg").innerHTML = "YOU WIN! YOU FOUND THE TREASURE"
+         document.getElementById("result").innerHTML = "YOU WIN! YOU FOUND THE TREASURE"
      }
      else if (location === trap1Location) {
        document.getElementById(location).innerHTML = "☠️";
-       document.getElementById("arg").innerHTML = "You fell in a trap!  Try Again?";
+       document.getElementById("result").innerHTML = "You fell in a trap!  Try Again?";
+     }
+     else if(counter == 0){
+       document.getElementById('table').innerHTML = "You didnt find the treasure in time. Try again!"
      }
      else {
+       counter --
        document.getElementById(location).innerHTML = "✘";
      }
  }
